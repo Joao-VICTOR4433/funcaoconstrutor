85f595a8977f1e666ebb195dbf3805df38d66583
joão victor <joao.nogueira@dcx.ufpb.br>
2025-09-05 19:07:43 -0300

    Joption

---
+++ src/main/java/org/example/espanha.java
@@ -0,0 +1,8 @@
+package org.example;
+import javax.swing.JOptionPane;
+public class espanha {
+    public static void main(String[]args){
+        JOptionPane.showMessageDialog(null,"Welcome João Victor!");
+
+    }
+}
